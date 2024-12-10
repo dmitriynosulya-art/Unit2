b232a91f0772eff17341a8116d1620d46eef1d4f
dmitriy_nosulya <d.nosulya@carx-tech.com>
2024-12-10 08:55:21 +0300

    unit 2.2 Classes

--- Unit2/Program.cs
+++ Unit2/Program.cs
@@ -8,22 +8,24 @@ namespace Unit2
 {
     internal class Program
     {
+        class House 
+        {
+            public string street;
+            public int number;
+
+            public void Print()
+            {
+                Console.WriteLine($"{street}, {number}");
+            }
+        }
         static void Main(string[] args)
         {
-            void HelloWorld()
-            {
-                Console.WriteLine("Hello World");
-            }
-            void Sum(int a, int b) 
-            {
-                int sum = a + b;
-                Console.WriteLine(sum);
-            }
-            void human(string name, int age)
-            {
-                Console.WriteLine($"{name}, {age}");
-            }
-            human("Дмитрий", 33);
+            House tower = new House();
+            tower.street = "Красная";
+            tower.number = 50;
+
+            tower.Print();
+        
            
         }
     }
